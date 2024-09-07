df1088e121cfb9f6e0ef07d4b118d2711ad2e606
Anthony Xavier Florez Mejia <anthonyflorezx@gmail.com>
2024-09-07 15:00:14 -0500

    Se agrega opcion para editar

---
+++ src/app/modal-form-neiborhood/modal-form-neiborhood.component.spec.ts
@@ -0,0 +1,13 @@
+import { ModalFormNeiborhoodComponent } from './modal-form-neiborhood.component';
+
+describe('ModalFormNeiborhoodComponent', () => {
+  let component: ModalFormNeiborhoodComponent;
+
+  beforeEach(() => {
+    component = new ModalFormNeiborhoodComponent();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/modal-form-neiborhood/modal-form-neiborhood.component.ts
@@ -0,0 +1,32 @@
+import { CommonModule } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+@Component({
+  selector: 'app-modal-form-neiborhood',
+  templateUrl: './modal-form-neiborhood.component.html',
+  styleUrl: './modal-form-neiborhood.component.css',
+  standalone:true,
+  imports:[MatFormFieldModule, CommonModule,MatDialogModule, FormsModule,MatInputModule,MatButtonModule]
+
+  })
+export class ModalFormNeiborhoodComponent {
+  constructor(
+    public dialogRef: MatDialogRef<ModalFormNeiborhoodComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {
+    console.log(data)
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  saveChanges(): void {
+    // Cierra el modal y pasa los datos modificados
+    this.dialogRef.close(this.data);
+  }
+}
