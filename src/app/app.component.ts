475bc331cf75772a6580f5fafe5ac0a7bbff4265
Anthony Xavier Florez Mejia <anthonyflorezx@gmail.com>
2024-09-17 18:38:12 -0500

    Se agrega spinner y se deja un solor servicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,108 +13,84 @@ import { catchError, Observable, throwError } from 'rxjs';
 import { ModalFormNeiborhoodComponent } from './modal-form-neiborhood/modal-form-neiborhood.component';
 import { MatDialog } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
-
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 // barrio.interface.ts
 export interface Barrio {
-  Id:number;
+  Id: number;
   Name: string;
   Price: string | number;
-  Idzone: number
+  Idzone: number;
 }
+
+// zona.interface.ts
 export interface Zona {
   Id: number;
-  Name:string
-  Idtabulador:number
-
+  Name: string;
+  Idtabulador: number;
+  Neiborhood: Barrio[];
 }
-
-
 
 // tabulador.interface.ts
 export interface Tabulador {
-  Id:number;
+  Id: number;
   Name: string;
+  Zones: Zona[];
 }
-
-
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,
-    FormsModule,CommonModule, 
-    MatGridListModule,  MatFormFieldModule,HttpClientModule,
+    FormsModule, CommonModule,
+    MatGridListModule, MatFormFieldModule, HttpClientModule,MatProgressSpinnerModule,
     MatButtonModule,
-    MatSelectModule,MatIconModule,
+    MatSelectModule, MatIconModule,
     MatListModule,
     MatCardModule,],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 
-    tabuladors:Tabulador[] = []
-    zona:Zona[] = []
-    barrio:Barrio[] = []
-
-
-    zonasDisponibles: Zona[] = [];
+  tabuladors: Tabulador[] = [];
+  zonasDisponibles: Zona[] = [];
   barriosDisponibles: Barrio[] = [];
   selectedBarrio?: Barrio;
+  loading: boolean = true;  // Variable para mostrar el spinner
+
+
   private tabuladoresUrl = 'https://microserviciouser.azurewebsites.net/api/tabulators';
-  private zonasUrl = 'https://microserviciouser.azurewebsites.net/api/zones';
-  private barriosUrl = 'https://microserviciouser.azurewebsites.net/api/neiborhoods';
   private apiKey = 'DJl40Xh-wOlVqJHEEekC6Ys-XWucJgnCcdh2LluBn358AzFuXCKIfw==';  // Reemplaza 'TU_CLAVE_AQUI' por tu clave real
 
   constructor(private http: HttpClient, public dialog: MatDialog) {
-       // Cargar los datos de tabuladores, zonas y barrios
-       this.getTabuladores().subscribe(tabuladores => {
-        this.tabuladors = tabuladores;
-      });
-  
-      this.getZonas().subscribe(zonas => {
-        this.zona = zonas;
-      });
-  
-      this.getBarrios().subscribe(barrios => {
-        this.barrio = barrios;
-      });
+    // Cargar los datos de tabuladores
+    this.getTabuladores().subscribe(tabuladores => {
+      this.tabuladors = tabuladores;
+      this.loading = false;  // Oculta el spinner cuando los datos están cargados
 
+    });
   }
-  
+
   onTabuladorChange(event: MatSelectChange) {
-    console.log("a");
-    
     const tabuladorId = event.value;
-    console.log(tabuladorId);
-    
-    this.zonasDisponibles = this.zona.filter(z => z.Idtabulador === tabuladorId);
+    this.zonasDisponibles = this.tabuladors.find(t => t.Id === tabuladorId)?.Zones || [];
     this.barriosDisponibles = []; // Limpiar los barrios cuando cambie el tabulador
-  }
-  onBarrioSelect(barrio: Barrio) {
-    this.selectedBarrio = barrio;
   }
 
   onZonaChange(event: MatSelectChange) {
     const zonaId = event.value;
-    console.log("zona",zonaId);
-    
-    this.barriosDisponibles = this.barrio.filter(b => b.Idzone === zonaId);
+    const selectedZona = this.zonasDisponibles.find(z => z.Id === zonaId);
+    this.barriosDisponibles = selectedZona ? selectedZona.Neiborhood : [];
+  }
+
+  onBarrioSelect(barrio: Barrio) {
+    this.selectedBarrio = barrio;
   }
 
   getTabuladores(): Observable<Tabulador[]> {
     return this.http.get<Tabulador[]>(this.tabuladoresUrl, { headers: this.getHeaders() });
   }
-
-  getZonas(): Observable<Zona[]> {
-    return this.http.get<Zona[]>(this.zonasUrl, { headers: this.getHeaders() });
-  }
-
-  getBarrios(): Observable<Barrio[]> {
-    return this.http.get<Barrio[]>(this.barriosUrl,{ headers: this.getHeaders() });
-  }
-  
 
   private getHeaders(): HttpHeaders {
     return new HttpHeaders({
@@ -122,49 +98,49 @@ export class AppComponent {
     });
   }
 
-    // Abrir el modal de edición
-    openEditDialog(barrio: Barrio): void {
-      const dialogRef = this.dialog.open(ModalFormNeiborhoodComponent, {
-        width: '300px',
-        data: { ...barrio }  // Clonamos el objeto barrio
-      });
-  
-      dialogRef.afterClosed().subscribe((result:any) => {
-        if (result) {
-          // Aquí llamamos a la API para actualizar el barrio
-          this.updateBarrio(result);
-        }
-      });
-    }
+  // Abrir el modal de edición
+  openEditDialog(barrio: Barrio): void {
+    const dialogRef = this.dialog.open(ModalFormNeiborhoodComponent, {
+      width: '300px',
+      data: { ...barrio }  // Clonamos el objeto barrio
+    });
 
-    updateBarrio(barrio: Barrio): void {
-      this.updateBarrioService(barrio).subscribe({next:(response) => {
+    dialogRef.afterClosed().subscribe((result: any) => {
+      if (result) {
+        // Aquí llamamos a la API para actualizar el barrio
+        this.updateBarrio(result);
+      }
+    });
+  }
+
+  updateBarrio(barrio: Barrio): void {
+    this.updateBarrioService(barrio).subscribe({
+      next: (response) => {
         // Actualiza la lista de barrios disponibles con los cambios
-
-        this.getBarrios().subscribe(barrios => {
-          this.barrio = barrios;
-          const index = this.barriosDisponibles.findIndex(b => b.Id === barrio.Id);
-          if (index !== -1) {
-            this.barriosDisponibles[index] = barrio;
+        this.getTabuladores().subscribe(tabuladores => {
+          this.tabuladors = tabuladores;
+          const selectedZona = this.zonasDisponibles.find(z => z.Id === barrio.Idzone);
+          if (selectedZona) {
+            const index = selectedZona.Neiborhood.findIndex(b => b.Id === barrio.Id);
+            if (index !== -1) {
+              selectedZona.Neiborhood[index] = barrio;
+            }
           }
         });
-   
-      }});
-    }
+      }
+    });
+  }
 
-    updateBarrioService(barrio: Barrio): Observable<Barrio> {
-      const url = `https://microserviciouser.azurewebsites.net/api/neiborhoods/${barrio.Id}`;
-      const headers = this.getHeaders();
-      return this.http.put<Barrio>(url, barrio, { headers })
-        .pipe(catchError(this.handleError));
-    }
-    
-    private handleError(error: HttpErrorResponse) {
-      console.error('Error en la API:', error);
-      return throwError('Algo salió mal; por favor intenta de nuevo más tarde.');
-    }
+  updateBarrioService(barrio: Barrio): Observable<Barrio> {
+    const url = `https://microserviciouser.azurewebsites.net/api/neiborhoods/${barrio.Id}`;
+    const headers = this.getHeaders();
+    return this.http.put<Barrio>(url, barrio, { headers })
+      .pipe(catchError(this.handleError));
+  }
 
-    
-
+  private handleError(error: HttpErrorResponse) {
+    console.error('Error en la API:', error);
+    return throwError('Algo salió mal; por favor intenta de nuevo más tarde.');
+  }
 
 }
