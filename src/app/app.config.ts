9837a079b21f2c73dde32769bb5d7fe61d692779
Anthony Xavier Florez Mejia <anthony1444>
2025-01-07 15:27:26 -0500

    Se agrega para que funcione con pwa

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -12,5 +12,8 @@ export const appConfig: ApplicationConfig = {
       provideServiceWorker('ngsw-worker.js', {
         enabled: environment.production,
         registrationStrategy:'registerWhenStable:30000'
-      })]
+      }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })]
 };
