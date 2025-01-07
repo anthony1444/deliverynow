2adac233c0ea3aa186ecbac812dec3b0b6ab544e
Anthony Xavier Florez Mejia <anthony1444>
2025-01-07 15:48:11 -0500

    Limpiar APIKEYNOTIFICATION

--- src/app/shared/services/notification/notification.service.ts
+++ src/app/shared/services/notification/notification.service.ts
@@ -40,7 +40,7 @@ export class NotificationService {
 
     try {
       const subscription = await this.swPush.requestSubscription({
-        serverPublicKey: this.VAPID_PUBLIC_KEY,
+        serverPublicKey: this.cleanVapidKey(this.VAPID_PUBLIC_KEY),
       });
 
       console.log('Successfully subscribed to notifications:', subscription);
@@ -80,4 +80,19 @@ export class NotificationService {
       throw error;
     }
   }
+  private urlBase64ToUint8Array(base64String: string): Uint8Array {
+    const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
+    const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/');
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+    for (let i = 0; i < rawData.length; ++i) {
+      outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+  }
+
+  private cleanVapidKey(key: string): string {
+    return key.replace(/[\s]/g, ''); // Elimina espacios
+  }
+
 }
